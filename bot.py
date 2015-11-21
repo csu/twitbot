bc3998bd90ea00b001463a335dc747aef5a7620a
Christopher Su <christophersu9@gmail.com>
2015-11-20 23:26:25 -0800

    bot

--- bot.py
+++ bot.py
@@ -1,4 +1,17 @@
+import tweepy
+
 from modularity import modularity
 
-for p in modularity.get_modules(config.MODULES_DIR):
-    # do stuff
+class Twitbot(object):
+    class StreamListener(tweepy.StreamListener):
+        def on_status(self, status):
+            pass
+
+    def __init__(self, api_key, api_secret, auth_token, auth_secret):
+        auth = tweepy.OAuthHandler(api_key, api_secret)
+        auth.set_access_token(auth_token, auth_secret)
+        self.api = tweepy.API(auth)
+
+        stream_listener = StreamListener()
+        stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+        stream.userstream(_with='user', replies='all')
