6867c96968b3aa8d5969a5e2df048e7468fc9c79
Christopher Su <christophersu9@gmail.com>
2015-11-21 23:43:58 -0800

    stuff

--- bot.py
+++ bot.py
@@ -8,10 +8,12 @@ class Twitbot(object):
             pass
 
     def __init__(self, api_key, api_secret, auth_token, auth_secret):
+        # Set up Tweepy
         auth = tweepy.OAuthHandler(api_key, api_secret)
         auth.set_access_token(auth_token, auth_secret)
         self.api = tweepy.API(auth)
 
+        # Set up stream listener
         stream_listener = StreamListener()
         stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
         stream.userstream(_with='user', replies='all')
