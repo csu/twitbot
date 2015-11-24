91f93320a3a140e36587efa6592f1f37fadee9f0
Christopher Su <christophersu9@gmail.com>
2015-11-24 14:17:28 -0800

    module loading working

--- bot.py
+++ bot.py
@@ -1,6 +1,29 @@
 import tweepy
-
+from ttp import ttp
 from modularity import modularity
+
+import secrets
+
+parser = ttp.Parser()
+
+def parse_tweet(parser, tweet_from, tweet_text):
+    query = tweet_text[tweet_text.index('@%s' % secrets.bot_username) + len('@%s' % secrets.bot_username) + 1:]
+
+    result = parser.parse(tweet_text)
+    mentions = result.users + [tweet_from]
+    while secrets.bot_username in mentions:
+        mentions.remove(secrets.bot_username)
+    tagged_hashtags = result.tags
+    tagged_urls = result.urls
+
+    for user in mentions:
+        query = query.replace('@%s' % user, '')
+    for tag in tagged_hashtags:
+        query = query.replace('#%s' % tag, '')
+    for url in tagged_urls:
+        query = query.replace('%s' % url, '')
+
+    return mentions, query.strip()
 
 class Twitbot(object):
     class TwitbotStreamListener(tweepy.StreamListener):
@@ -10,15 +33,38 @@ class Twitbot(object):
             self.listener_modules = listener_modules
 
         def on_status(self, status):
-            pass
+            tweet_from = status.user.screen_name
 
-    def __init__(self, api_key, api_secret, auth_token, auth_secret):
+            if (tweet_from != secrets.bot_username and
+                tweet_from in secrets.whitelist and
+                not hasattr(status, 'retweeted_status')):
+
+                tweet_text = status.text
+                mentions, tweet_text = parse_tweet(parser, tweet_from, tweet_text)
+                mentions = ['@%s' % x for x in mentions]
+
+                for mod in self.listener_modules:
+                    m = getattr(mod, 'process_tweet')
+                    result = m(tweet_text, mentions, self.api)
+
+    def __init__(self, api_key, api_secret, auth_token, auth_secret, modules=[]):
         # Set up Tweepy
         auth = tweepy.OAuthHandler(api_key, api_secret)
         auth.set_access_token(auth_token, auth_secret)
         self.api = tweepy.API(auth)
+        self.modules = modules
 
+    def listen(self):
         # Set up stream listener
-        stream_listener = TwitbotStreamListener()
-        stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+        stream_listener = self.TwitbotStreamListener()
+        stream_listener.set_modules(self.modules)
+        stream = tweepy.Stream(auth=self.api.auth, listener=stream_listener)
         stream.userstream(_with='user', replies='all')
+
+if __name__ == "__main__":
+    bot = Twitbot(secrets.twitter['api_key'],
+                secrets.twitter['api_secret'],
+                secrets.twitter['auth_token'],
+                secrets.twitter['auth_secret'],
+                modules=modularity.get_imported('modules'))
+    bot.listen()
---
+++ modules/karebear/karebear.py
@@ -0,0 +1,5 @@
+def process_tweet(tweet_text, mentions, api):
+    pass
+    # content = '%s %s' % (' '.join(mentions), response)
+    # tweet_id = status.id
+    # api.update_status(status=content, in_reply_to_status_id=tweet_id)
