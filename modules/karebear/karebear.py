f0637fd48db760cb77b438af3c3e09bf9e5cbea4
Christopher Su <christophersu9@gmail.com>
2015-11-24 14:18:56 -0800

    gitignore

--- modules/karebear/karebear.py
+++
@@ -1,5 +0,0 @@
-def process_tweet(tweet_text, mentions, api):
-    pass
-    # content = '%s %s' % (' '.join(mentions), response)
-    # tweet_id = status.id
-    # api.update_status(status=content, in_reply_to_status_id=tweet_id)
